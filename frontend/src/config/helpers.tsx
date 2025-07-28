810c1e1bd3524183cad3e4dcfb1b9838ba7ff39b
Madni <thezubairkhan.developer@gmail.com>
2025-07-28 20:05:45 +0500

    updated

--- frontend/src/config/helpers.tsx
+++ frontend/src/config/helpers.tsx
@@ -5,7 +5,7 @@ class Helpers {
   static localhost: string = "http://localhost:8080";
   static server: string = "https://api.legalmasterai.com/backend";
   static payment: string = `${Helpers.server}`;
-  static basePath: string = Helpers.localhost;
+  static basePath: string = Helpers.server;
   static apiUrl: string = `${Helpers.basePath}/api/v1`;
 
   static authUser: Record<string, any> = JSON.parse(
